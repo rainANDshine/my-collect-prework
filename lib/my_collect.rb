fcc8e9d202feefa103045cba039435ee074b371a
Shun Yao <ysmiracle@hotmail.com>
2018-03-28 23:31:53 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,12 @@
 def my_collect(collection)
   i = 0
+  array = []
   
   while i < collection.length
-    yield collection[i]
+    array << (yield collection[i])
     i += 1
   end
   
-  
+  array
 end
 
